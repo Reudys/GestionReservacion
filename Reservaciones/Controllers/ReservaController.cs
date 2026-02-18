93a1e7c7a764f93a9a682240331491e82be2ba47
Reudys <reudysfernando@gmail.com>
2026-02-18 10:40:31 -0400

    Crud Reservaciones completo

--- Reservaciones/Controllers/ReservaController.cs
+++ Reservaciones/Controllers/ReservaController.cs
@@ -13,26 +13,44 @@ namespace Reservaciones.Controllers
 
         public ReservaController(IReservaService reservaService) { _service = reservaService; }
 
-        [HttpPost]
+        #region CRUD Methods
+        [HttpPost("Create")]
         public async Task<IActionResult> CreateReserva(Reserva reserva)
         {
             await _service.CreateReservaAsync(reserva);
             return Ok();
         }
 
-        [HttpGet]
+        [HttpGet("Read")]
         public async Task<IActionResult> GetReservas()
         {
             var reservas = await _service.GetReservasAsync();
             return Ok(reservas);
         }
+        [HttpPut("Update/{id}")]
+        public async Task<IActionResult> UpdateReserva(int id, Reserva reserva)
+        {
+            var existingReserva = await _service.UpdateReservaAsync(id, reserva);
+            if (existingReserva == null) return NotFound();
+            return Ok(existingReserva);
+        }
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> DeleteReserva(int id)
+        {
+            var deleted = await _service.DeleteReservaAsync(id);
+            if (!deleted) return NotFound();
+            return Ok();
+        }
+        #endregion
 
-        [HttpGet("{id}")]
+        #region Custom Methods
+        [HttpGet("Read/{id}")]
         public async Task<IActionResult> GetReservaById(int id)
         {
             var reserva = await _service.GetReservaByIdAsync(id);
             if (reserva == null) return NotFound();
             return Ok(reserva);
         }
+        #endregion
     }
 }
